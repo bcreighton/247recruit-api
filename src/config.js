2a2b84f61b8708f57ea50f4e99ee33992628cca6
Bryson <brysoncreighton@gmail.com>
2021-01-18 14:26:52 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://bryson:creativedev@localhost/247recruit',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postegresql://bryson:creativedev@localhost/247recruit-test',
   API_TOKEN: '438fed41-e90f-49bb-ab1a-1c946f3fc696',
 }
