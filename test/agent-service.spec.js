b6f37116c320fd7fc58f72e73f68ee84179e7209
Bryson Creighton <brysoncreighton@gmail.com>
2021-01-19 01:40:53 -0700

    update agent service test

--- test/agent-service.spec.js
+++ test/agent-service.spec.js
@@ -16,44 +16,16 @@ describe(`Agent service object`, () => {
         });
     });
 
-    // Clear all data from the db tables
-
     before(() => dbTableTransactions.cleanDB(db));
-
-    // Clear all data from the db tables after each test
-
     afterEach(() => dbTableTransactions.cleanDB(db));
-    
     after(() => db.destroy());
 
     context(`Given 'agents' has data`, () => {
-        // insert test data into the appropriate
+        // insert necessary data for agent table requirements
         before(() => {
-            return (
-                db
-                .into('agents')
-                .insert(testAgents)
-                .then(() => {
-                    return db.into('brokerages')
-                    .insert(testBrokerages)
-                })
-            )
-        });
-
-        before(() => {
-            return db('agents')
-                .where('id', '=', 1)
-                .update('brokerage', 1)
-                .then(() => {
-                    return db('agents')
-                        .where('id', '=', 2)
-                        .update('brokerage', 2)
-                })
-                .then(() => {
-                    return db('agents')
-                        .where('id', '=', 3)
-                        .update('brokerage', 3)
-                });
+            return dbTableTransactions.insertAgentData(db)
+            .then(() => dbTableTransactions.insertBrokerageData(db))
+            .then(() => dbTableTransactions.alterAgentsBrokerageFkey(db))
         });
 
         it(`getAgents() resolves all agents from the 'agents' table`, () => {
