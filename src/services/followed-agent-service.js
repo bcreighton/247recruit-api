2e7cb21a29be8485e2409ac01bdfc18b542a1ec3
Bryson Creighton <brysoncreighton@gmail.com>
2021-01-19 18:05:50 -0700

    Update followed agents service test
    Implemented service methods to get all followed agent/user pairs, get followed agents for a specific user, and delete a followed agent pair.

--- src/services/followed-agent-service.js
+++ src/services/followed-agent-service.js
@@ -1,6 +1,29 @@
 const FollowedAgentService = {
     getFollowedAgents(knex) {
         return knex.select('*').from('followed_agents')
+    },
+
+    getByUsernameId(knex, id) {
+        return knex
+            .from('followed_agents')
+            .select('*')
+            .where('username_id', id)
+    },
+    insertFollowedAgent(knex, newFollowedAgent) {
+        return knex
+            .insert(newFollowedAgent)
+            .into('followed_agents')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    deleteFollowedAgent(knex, usernameId, agentId) {
+        return knex('followed_agents')
+            .where({
+                agent_id: agentId, 
+                username_id: usernameId
+            }).delete()
     }
 };
 
--- test/followed-agent-service.spec.js
+++ test/followed-agent-service.spec.js
@@ -4,7 +4,7 @@ const {testAgents, testBrokerages, testUsers, testNotes, testFollowedAgents} = r
 const dbTableTransactions = require('./db-table-transactions');
 const knex = require('knex');
 
-describe.only(`Followed Agent service object`, () => {
+describe(`Followed Agent service object`, () => {
     let db;
 
     // Establish db connection
@@ -22,7 +22,7 @@ describe.only(`Followed Agent service object`, () => {
 
     context(`Given 'followed_agents' has data`, () => {
         // insert necessary data for db table requirements
-        before(() => {
+        beforeEach(() => {
             return dbTableTransactions.insertAgentData(db)
             .then(() => dbTableTransactions.insertBrokerageData(db))
             .then(() => dbTableTransactions.insertUserData(db))
@@ -32,24 +32,83 @@ describe.only(`Followed Agent service object`, () => {
         });
 
         it(`getFollowedAgents() resolves all followed agents from the 'followed_agents' table`, () => {
-            testAgents.map((a, n = 1) => {
-                let kv = {brokerage: parseInt(`${n+1}`)};
-                let obj = {...a, ...kv};
-                return obj;
-            }) 
             return FollowedAgentService.getFollowedAgents(db)
                 .then(actual => {
                     expect(actual).to.eql(testFollowedAgents);
                 });
         });
+
+        it(`getById() resolves all followed agents by username_id from the 'followed_agents' table`, () => {
+            const thirdId = 3;
+            const thirdTestUserFollowedAgents = [
+                {
+                    agent_id: 3,
+                    username_id: 3
+                },
+                {
+                    agent_id: 2,
+                    username_id: 3
+                }
+            ]
+
+            return FollowedAgentService.getByUsernameId(db, thirdId)
+                .then(actual => {
+                    expect(actual).to.eql(thirdTestUserFollowedAgents)
+                })
+        })
+
+        it(`deleteFollowedAgent() removes a followed agent by username and agent id from the 'followed_agents' table`, () => {
+            const filter = {
+                username_id: 3,
+                agent_id: 2
+            };
+            const { username_id, agent_id } = filter;
+
+            return FollowedAgentService.deleteFollowedAgent(db, username_id, agent_id)
+                .then(() => FollowedAgentService.getFollowedAgents(db))
+                .then(allFollowedAgents => {
+                    debugger;
+                    const expected = testFollowedAgents.filter(a => {
+                        for (let key in filter) {
+                            if (a[key] === undefined || a[key] != filter[key])
+                                return true;
+                        }
+                        return false;
+                    })
+                    expect(allFollowedAgents).to.eql(expected)
+                })
+        })
     });
 
     context(`Given 'followed_agents' has no data`, () => {
+        // insert necessary data for db table requirements
+        beforeEach(() => {
+            return dbTableTransactions.insertAgentData(db)
+            .then(() => dbTableTransactions.insertBrokerageData(db))
+            .then(() => dbTableTransactions.insertUserData(db))
+            .then(() => dbTableTransactions.alterAgentsBrokerageFkey(db))
+        });
+
         it(`getFollowedAgents() resolves an empty array`, () => {
             return FollowedAgentService.getFollowedAgents(db)
                 .then(actual => {
                     expect(actual).to.eql([])
                 });
         });
+
+        it(`insertFollowedAgent() inserts a followed agent and resolves with the new note with an object`, () => {
+            const newFollowedAgent = {
+                agent_id: 3,
+                username_id: 1
+            }
+
+            return FollowedAgentService.insertFollowedAgent(db, newFollowedAgent)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        agent_id: newFollowedAgent.agent_id,
+                        username_id: newFollowedAgent.username_id
+                    })
+                })
+        })
     });
 });
