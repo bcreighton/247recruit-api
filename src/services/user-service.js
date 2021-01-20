6cdc684971586ceba5c3060e067a50c152bccf8c
Bryson Creighton <brysoncreighton@gmail.com>
2021-01-19 18:08:03 -0700

    Update user service test
    Implement service methods and tests to get all users, get a specific user by id, update a user and delete a user.

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,36 @@
 const UserService = {
     getUsers(knex) {
         return knex.select('*').from('users')
+    },
+
+    getById(knex, id) {
+        return knex
+            .from('users')
+            .select('*')
+            .where('id', id)
+            .first()
+    },
+
+    insertUser(knex, newUser) {
+        return knex
+            .insert(newUser)
+            .into('users')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+
+    updateUser(knex, id, newUserFields) {
+        return knex('users')
+            .where({ id })
+            .update(newUserFields)
+    },
+
+    deleteUser(knex, id) {
+        return knex('users')
+            .where({ id })
+            .delete()
     }
 };
 
--- test/user-service.spec.js
+++ test/user-service.spec.js
@@ -1,5 +1,7 @@
 const { expect } = require("chai");
 const UserService = require('../src/services/user-service');
+const BrokerageService = require('../src/services/brokerage-service');
+const AgentService = require('../src/services/agent-service')
 const {testAgents, testBrokerages, testUsers} = require('./test-vars');
 const dbTableTransactions = require('./db-table-transactions');
 const knex = require('knex');
@@ -20,9 +22,9 @@ describe(`User service object`, () => {
     afterEach(() => dbTableTransactions.cleanDB(db));
     after(() => db.destroy());
 
-    context(`Given 'agents' has data`, () => {
-        // insert necessary data for agent table requirements
-        before(() => {
+    context(`Given 'users' has data`, () => {
+        // insert necessary data for brokerage table requirements
+        beforeEach(() => {
             return dbTableTransactions.insertAgentData(db)
             .then(() => dbTableTransactions.insertBrokerageData(db))
             .then(() => dbTableTransactions.insertUserData(db))
@@ -30,24 +32,100 @@ describe(`User service object`, () => {
         });
 
         it(`getUsers() resolves all users from the 'users' table`, () => {
-            testAgents.map((a, n = 1) => {
-                let kv = {brokerage: parseInt(`${n+1}`)};
-                let obj = {...a, ...kv};
-                return obj;
-            }) 
             return UserService.getUsers(db)
                 .then(actual => {
                     expect(actual).to.eql(testUsers);
                 });
         });
+
+        it(`getById() resolves a user by id from the 'users' table`, () => {
+            const thirdId = 3;
+            const thirdTestUser = testUsers[thirdId -1];
+
+            return UserService.getById(db, thirdId)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: thirdId,
+                        username: thirdTestUser.username,
+                        first_name: thirdTestUser.first_name,
+                        last_name: thirdTestUser.last_name,
+                        email: thirdTestUser.email,
+                        phone: thirdTestUser.phone,
+                        brokerage: thirdTestUser.brokerage
+                    })
+                })
+        })
+
+        it(`updateUser() updates a user from the 'users' table`, () => {
+            const idOfUserToUpdate = 3;
+            const newUserData = {
+                email: "srogan250@springer.com",
+                phone: "927-708-4768",
+                brokerage: 3
+            }
+
+            return UserService.updateUser(db, idOfUserToUpdate, newUserData)
+                .then(() => UserService.getById(db, idOfUserToUpdate))
+                .then(user => {
+                    expect(user).to.eql({
+                        id: idOfUserToUpdate,
+                        username: user.username,
+                        first_name: user.first_name,
+                        last_name: user.last_name,
+                        ...newUserData
+                    })
+                })
+        })
+
+        it(`deleteUser() removes a user by id from the 'users' table`, () => {
+            const userId = 3;
+
+            return UserService.deleteUser(db, userId)
+                .then(() => UserService.getUsers(db))
+                .then(allUsers => {
+                    const expected = testUsers.filter(u => u.id !== userId);
+                    expect(allUsers).to.eql(expected)
+                })
+        })
     });
 
     context(`Given 'users' has no data`, () => {
+        // insert necessary data for brokerage table requirements
+        beforeEach(() => {
+            return dbTableTransactions.insertAgentData(db)
+            .then(() => dbTableTransactions.insertBrokerageData(db))
+            .then(() => dbTableTransactions.alterAgentsBrokerageFkey(db))
+        })
+
         it(`getUsers() resolves an empty array`, () => {
             return UserService.getUsers(db)
                 .then(actual => {
                     expect(actual).to.eql([])
                 });
         });
+
+        it(`insertUser() inserts a new user and resolves the new user with an id`, () => {
+            const newUser = {
+                username: 'Test User',
+                first_name: 'Test',
+                last_name: 'User',
+                email: 'testuser@testuser.com',
+                phone: '303-111-1111',
+                brokerage: 2
+            };
+
+            return UserService.insertUser(db, newUser)
+                .then(actual => {
+                    expect(actual).that.eql({
+                        id: 1,
+                        username: newUser.username,
+                        first_name: newUser.first_name,
+                        last_name: newUser.last_name,
+                        email: newUser.email,
+                        phone: newUser.phone,
+                        brokerage: newUser.brokerage
+                    })
+                })
+        })
     });
 });
