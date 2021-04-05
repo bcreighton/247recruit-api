5b3858f8068dbbf8bb4841cf0e53ad6ce913c10b
Bryson Creighton <brysoncreighton@gmail.com>
2021-04-05 16:38:37 -0600

    Added bcrypt
    Updated password handling to use bcrypt for better encryption and security of data.

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const AuthService = require('../services/auth-service');
 
 requireAuth = (req, res, next) => {
@@ -23,12 +24,17 @@ requireAuth = (req, res, next) => {
     
     AuthService.getUsername(req.app.get('db'),tokenUsername)
         .then(user => {
-            if (!user || user.password !== tokenPassword) {
+            if (!user) {
                 return res.status(401).json({ error: 'Unauthorized request'})
             }
 
-            req.user = user;
-            next()
+            return bcrypt.compare(tokenPassword, user.password)
+                .then(passwordsMatch => {
+                    (!passwordsMatch) && res.status(401).json({ error: 'Unauthorized request'})
+
+                    req.user = user;
+                    next()
+                })
         })
         .catch(next)
 }
